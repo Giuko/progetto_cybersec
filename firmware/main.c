c3fad23a75e287d5b26821d5933b4202b4249515
silviaB24 <silvia.bonenti@gmail.com>
2025-05-06 09:55:28 +0000

    start command header validation

--- firmware/main.c
+++ firmware/main.c
@@ -1,7 +1,27 @@
 #include "uart.h"
+#include "tpm.h"
+
+uint8_t command_buffer[] = {
+    0x80, 0x01,  // TPM_ST_NO_SESSIONS
+    0x00, 0x00, 0x00, 0x0C,  // commandSize = 12 bytes
+    0x00, 0x00, 0x01, 0x53   // TPM2_Hash command code
+};
+
 int main(void){
     int i = 5;
     UART_init();
     UART_printf("Hello board\n");
+
+
+
+    uint32_t rc = validate_command_header(command_buffer, sizeof(command_buffer));
+    if (rc == TPM_RC_SUCCESS) {
+        UART_printf("Command header is valid\n");
+    } else {
+        char error_message[50];
+        snprintf(error_message, sizeof(error_message), "Error: 0x%X\n", rc);
+        UART_printf(error_message);
+    }
+
     return 0;
 }
---
+++ firmware/tpm.h
@@ -0,0 +1,20 @@
+#ifndef __TPMH__
+#define __TPMH__
+
+#include <stdint.h>
+
+#define TPM_ADDRESS                         ( 0xE0000000UL )
+#define TPM_SIZE                            ( 4 * 1024 )
+
+#define TPM_RC_SUCCESS            0x000
+#define TPM_RC_BAD_TAG            0x01E
+#define TPM_RC_COMMAND_SIZE       0x01D
+#define TPM_RC_COMMAND_CODE       0x014
+
+#define TPM_ST_NO_SESSIONS        0x8001
+#define TPM_ST_SESSIONS           0x8002
+
+void is_command_supported(TPM_CC);
+uint32_t validate_command_header(const uint8_t, size_t );
+
+#endif
